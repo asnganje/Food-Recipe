c516029f571c9b3378d17ce59bc150c1cfcd4ee4
asnganje <abd.nganje@gmail.com>
2023-02-13 21:30:32 +0300

    fix linter errors

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,5 +1,5 @@
 class Food < ApplicationRecord
-    has_many :recipe_foods
-    has_many :recipes, through: :recipe_foods  
-    belongs_to :user
+  has_many :recipe_foods
+  has_many :recipes, through: :recipe_foods
+  belongs_to :user
 end
