28082ed5c7274cac6406e833a59f5b3de10d1615
Odette Gleen <nkargleeny@gmail.com>
2023-02-14 16:56:50 +0100

    added the navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :recipe_foods
-  resources :recipes
+  resources :recipes 
   resources :foods
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
