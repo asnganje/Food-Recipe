270e778ab2fa39b5c070e2eaae2740f0700d131e
asnganje <abd.nganje@gmail.com>
2023-02-15 06:58:13 +0300

    fix linter erros

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,6 +1,5 @@
 class PublicRecipesController < ApplicationController
-
-    def index
-        @public_recipes = Recipe.where(public: true).includes(:user, :recipe_foods, :foods)
-    end
+  def index
+    @public_recipes = Recipe.where(public: true).includes(:user, :recipe_foods, :foods)
+  end
 end
